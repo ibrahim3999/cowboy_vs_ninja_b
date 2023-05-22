073385b3499445449e980c8f7e7fa1f53a80f275
ibrahim mjadly <48316131+ibrahim3999@users.noreply.github.com>
2023-05-22 21:52:55 +0300

    Update OldNinja.cpp

--- sources/OldNinja.cpp
+++ sources/OldNinja.cpp
@@ -5,6 +5,7 @@ namespace ariel{
 
     OldNinja::OldNinja(string name ,const Point& location):Ninja(name,location){
         setSpeed(8);
+        setHealth(150);
     }
     //OldNinja::~OldNinja(){}
 
